d5eccd02e5a6b4e3b02c1259462c44385666b2fe
Abhishek <abhishek.saxena@scopeworker.com>
2020-02-21 00:10:29 +0530

    swap divs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
-
+import { Component,OnInit,ViewChild ,ElementRef} from '@angular/core';
+import { timer } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-app';
+  subscribeTimer: number;
+  timeLeft: number = 180;
+  @ViewChild("left", { read: ElementRef, static: true }) leftDiv;
+  @ViewChild("right", { read: ElementRef, static: true }) rightDiv;
+  
+  swapDivs() {
+    let leftDiv = this.leftDiv.nativeElement;
+    let rightDiv = this.rightDiv.nativeElement;
+
+    leftDiv.insertBefore(rightDiv, leftDiv.childNodes[0]);
+ }
+
+  ngOnInit() {
+    const source = timer(1000, 2000);
+    const abc = source.subscribe(val => {
+      this.subscribeTimer = this.timeLeft - val;
+    });
+  }
 }
